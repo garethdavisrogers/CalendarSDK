1e0b58bac3c6cf59b66e59d421f797d6d82e39d8
Gareth <gareth@redovoting.io>
2024-11-02 15:30:33 -0400

    Another update.

--- CalendarModel.cs
+++ CalendarModel.cs
@@ -1,39 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 
 public class CalendarModel
 {
+    public DateTime Today { get; private set; } = DateTime.Today;
+    public List<List<CalendarDay>> Weeks { get; private set; }
+
+    public CalendarModel(int year, int month)
+    {
+        Weeks = GenerateWeeks(year, month);
+    }
+
     public List<List<CalendarDay>> GenerateWeeks(int year, int month)
     {
         var weeks = new List<List<CalendarDay>>();
         var days = new List<CalendarDay>();
 
-        // 1. Define the first and last days of the month
+        // Start with the first day of the month
         DateTime firstDayOfMonth = new DateTime(year, month, 1);
-        DateTime lastDayOfMonth = new DateTime(year, month, DateTime.DaysInMonth(year, month));
 
-        // 2. Determine the starting display day by finding the previous Sunday
-        DateTime startDisplayDay = firstDayOfMonth;
-        while (startDisplayDay.DayOfWeek != DayOfWeek.Sunday)
+        // Shift back to the previous Sunday if needed
+        DateTime currentDay = firstDayOfMonth;
+        while (currentDay.DayOfWeek != DayOfWeek.Sunday)
         {
-            startDisplayDay = startDisplayDay.AddDays(-1);
+            currentDay = currentDay.AddDays(-1);
         }
 
-        // 3. Populate the display starting from `startDisplayDay` for 42 days (6 weeks * 7 days)
-        DateTime currentDay = startDisplayDay;
-        for (int i = 0; i < 42; i++)
+        // Generate days and weeks until we reach the end of the month view
+        DateTime lastDayOfMonth = new DateTime(year, month, DateTime.DaysInMonth(year, month));
+        while (currentDay <= lastDayOfMonth || days.Count > 0)
         {
             days.Add(new CalendarDay { Date = currentDay });
-            currentDay = currentDay.AddDays(1);
 
-            // Group into weeks of 7 days
+            // When we have a full week, add it to weeks and reset days
             if (days.Count == 7)
             {
                 weeks.Add(new List<CalendarDay>(days));
                 days.Clear();
             }
+
+            currentDay = currentDay.AddDays(1);
+        }
+
+        // If there are remaining days after the last week, fill up to 7 days
+        while (days.Count > 0 && days.Count < 7)
+        {
+            days.Add(new CalendarDay { Date = currentDay });
+            currentDay = currentDay.AddDays(1);
+        }
+
+        if (days.Count == 7)
+        {
+            weeks.Add(days);
         }
 
         return weeks;
@@ -43,5 +62,4 @@ public class CalendarModel
     {
         public DateTime Date { get; set; }
     }
-
 }
